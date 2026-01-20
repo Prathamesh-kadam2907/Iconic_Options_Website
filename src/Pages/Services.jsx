0245fb7fcb6471f17eba1670f592481d3a10476a
Prathmesh Chalke <prathmesh@rbktechnologies.com>
2026-01-20 12:27:55 +0530

    Theme Code Added.

--- src/Pages/Services.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const Services = () => {
-  return (
-    <div className='pt-20'>Services</div>
-  )
-}
-
-export default Services
--- src/component/Subscriptions/PaymentCheckout.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const PaymentCheckout = () => {
-  return <div className="mt-40">PaymentCheckout</div>;
-};
-
-export default PaymentCheckout;
